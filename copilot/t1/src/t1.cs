dc063ef37ffe5452a869214155bd66a6a0f26217
Markvi268 <markku.viertokangas@edu.savonia.fi>
2024-03-18 18:44:26 +0200

    t1 copilot

--- copilot/t1/src/t1.cs
+++ copilot/t1/src/t1.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+/*
+Luo c# ohjelma: Lotossa arvotaan seitsemän (7) varsinaista numeroa ja yksi (1) lisänumero. Numerot ovat väliltä 1-40.
+
+Esittele kahdeksan (8) alkioinen lotto-taulukko ja arvo siihen lottonumerot (7 ensimmäistä
+on siis varsinaisia lottonumeroita ja viimeinen on lisänumero). Muista, että samaa numeroa
+ei saa tulla lottoriviin eli mieti miten voisit tarkistaa onko arvottu numero jo lottorivissä entuudestaan ja silloin sen tilalle pitää arpoa uusi numero.
+
+Tulosta lopuksi lottorivi seuraavasti:
+
+1   4   12   16   19   25   31   +   13
+
+HUOM!! seitsemän varsinaista numeroa on lajiteltu suuruusjärjestykseen! Mutta
+lisänumeroa ei tietenkään saa ottaa lajitteluun mukaan! Löytysköhän Array.Sort:sta sellainen
+ominaisuus, jolla tämän saisi ratkaistua? Voisi esimerkiksi kertoa, että mistä indeksistä eteenpäin lajittellaan niin ja niin monta alkiota...
+
+
+HUOM! Luovempi kysely
+*/
+class Lotto
+{
+    static void Main()
+    {
+        int[] lottoNumbers = new int[8];
+        Random rand = new Random();
+
+        // Arvotaan 7 varsinaista numeroa
+        for (int i = 0; i < 7; i++)
+        {
+            int number;
+            do
+            {
+                number = rand.Next(1, 41);
+            } while (Array.Exists(lottoNumbers, element => element == number));
+            lottoNumbers[i] = number;
+        }
+
+        // Järjestetään 7 ensimmäistä numeroa
+        Array.Sort(lottoNumbers, 0, 7);
+
+        // Arvotaan lisänumero niin, että se ei ole mikään varsinaisista numeroista
+        int extraNumber;
+        do
+        {
+            extraNumber = rand.Next(1, 41);
+        } while (Array.Exists(lottoNumbers, element => element == extraNumber));
+
+        lottoNumbers[7] = extraNumber;
+
+        // Tulostetaan lottorivi
+        for (int i = 0; i < 7; i++)
+        {
+            Console.Write(lottoNumbers[i] + "   ");
+        }
+        Console.WriteLine("+   " + extraNumber);
+    }
+}
