477d618fb9c896405c0de65e38a070124e7acfeb
Markvi268 <markku.viertokangas@edu.savonia.fi>
2024-03-25 17:02:52 +0200

    AIcodes, check int[] name

--- chatGPT/AItest06/src/testcode6.cs
+++ chatGPT/AItest06/src/testcode6.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Luo c# ohjelma: Lotossa arvotaan seitsemän (7) varsinaista numeroa ja yksi (1) lisänumero. Numerot ovat väliltä 1-40.
+
+Esittele kahdeksan (8) alkioinen lotto-taulukko ja arvo siihen lottonumerot (7 ensimmäistä
+on siis varsinaisia lottonumeroita ja viimeinen on lisänumero). Muista, että samaa numeroa
+ei saa tulla lottoriviin eli mieti miten voisit tarkistaa onko arvottu numero jo lottorivissä entuudestaan ja silloin sen tilalle pitää arpoa uusi numero.
+
+Tulosta lopuksi lottorivi seuraavasti:
+
+1   4   12   16   19   25   31   +   13
+
+HUOM!! seitsemän varsinaista numeroa on lajiteltu suuruusjärjestykseen! Mutta
+lisänumeroa ei tietenkään saa ottaa lajitteluun mukaan! Löytysköhän Array.Sort:sta sellainen
+ominaisuus, jolla tämän saisi ratkaistua? Voisi esimerkiksi kertoa, että mistä indeksistä eteenpäin lajittellaan niin ja niin monta alkiota...
+
+*/
+
+// Kysytty 25.3.2024 uusi chat
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] lottoNumbers = new int[8];
+
+        Random random = new Random();
+
+        // Generoidaan varsinaiset lottonumerot
+        for (int i = 0; i < 7; i++)
+        {
+            int randomNumber;
+            do
+            {
+                randomNumber = random.Next(1, 41); // Generoidaan numero väliltä 1-40
+            } while (Array.IndexOf(lottoNumbers, randomNumber) != -1); // Tarkistetaan, onko numero jo lottonumeroina
+
+            lottoNumbers[i] = randomNumber;
+        }
+
+        // Lajitellaan varsinaiset lottonumerot suuruusjärjestykseen
+        Array.Sort(lottoNumbers, 0, 7);
+
+        // Generoidaan lisänumero
+        lottoNumbers[7] = random.Next(1, 41);
+
+        // Tulostetaan lottorivi
+        for (int i = 0; i < 8; i++)
+        {
+            Console.Write(lottoNumbers[i]);
+
+            if (i < 7)
+            {
+                Console.Write("   "); // Lisätään välilyönti varsinaisten numeroiden välille
+            }
+            else
+            {
+                Console.Write("   +   "); // Lisätään välilyönnit varsinaisten numeroiden ja lisänumeron väliin
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ readwritecodefiles.py
@@ -0,0 +1,85 @@
+import os
+import re
+
+
+def readCode() -> tuple[dict[str, int], dict[str, int]]:
+    #path:str = 'chatGPT/AItest01/src/testcode1.cs'
+    occurences_ai:dict[str, int] = {}
+    occurences_students:dict[str, int] = {}
+
+    dir_list:list[str] = ['chatGPT','studentscodes']
+
+    for directory in os.listdir('.'):
+        if directory in dir_list:
+            #print(f'Found directory: {directory}')
+            os.chdir(directory)
+            for file in os.listdir('.'):
+               if os.path.isdir(file):
+                 #print(f'Found directory in {directory}: {file}')
+                 os.chdir(file)
+                 for file in os.listdir('.'):
+                     if os.path.isdir(file):
+                         if file == 'src':
+                             os.chdir(file)
+                             for file in os.listdir('.'):
+                                 if os.path.exists(os.getcwd()):
+                                     with open (os.getcwd() + f'/{file}', 'r') as file:
+                                         for line in file:
+                                             tulos = re.search(r'\bint\[\]\s+(\w+)\s*=', line)
+                                             tulos1 = re.search(r'\bint\[\,]\s+(\w+)\s*=', line)
+                                             #print(line)
+                                             if directory == 'chatGPT':
+                                                if tulos:
+                                                    if tulos.group(1) in occurences_ai:
+                                                        occurences_ai[tulos.group(1)] += 1
+                                                    else:
+                                                        occurences_ai[tulos.group(1)] = 1
+                                                if tulos1:
+                                                    if tulos1.group(1) in occurences_ai:
+                                                        occurences_ai[tulos1.group(1)] += 1
+                                                    else:
+                                                        occurences_ai[tulos1.group(1)] = 1
+
+                                             if directory == 'studentscodes':
+                                                if tulos:
+                                                    if tulos.group(1) in occurences_students:
+                                                        occurences_students[tulos.group(1)] += 1
+                                                    else:
+                                                        occurences_students[tulos.group(1)] = 1
+                                                if tulos1:
+                                                    if tulos1.group(1) in occurences_students:
+                                                        occurences_students[tulos1.group(1)] += 1
+                                                    else:
+                                                        occurences_students[tulos1.group(1)] = 1
+                                                 
+                                                 
+                                 os.chdir('..')
+                             os.chdir('..')
+            os.chdir('..')
+    return (occurences_ai, occurences_students)            
+                    
+                       
+def writeCode() -> None:
+    occurences_ai, occurences_students = readCode()
+    
+    with open ('results.txt','w') as f:
+        f.write('AI code:\n')
+        for key, value in occurences_ai.items():
+            f.write(f'{key}: {value}\n')
+        f.write('\n')
+        f.write('Students code:\n')
+        for key, value in occurences_students.items():
+            f.write(f'{key}: {value}\n')
+        f.write('\n')
+        f.write('Differences:\n')
+        for key, value in occurences_students.items():
+                if key not in occurences_ai:
+                    f.write(f'{key}: Students {value} AI: 0\n')
+                else:
+                    f.write(f'{key}: Students: {value} AI: {occurences_ai[key]}\n')
+
+            
+
+
+if __name__ == '__main__':
+    writeCode()
